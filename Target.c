089078d513b8b7249a166a3cd73acd667b4d70be
Potti.Priyanka <119730292+rajkumarpotti@users.noreply.github.com>
2022-12-19 20:27:58 +0530

    #include<stdio.h> int main() {     int l,m,n,o;     scanf("%d%d%d%d",&l,&m,&n,&o);     if(l,m,n,o>=10)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int l,m,n,o;
+    scanf("%d%d%d%d",&l,&m,&n,&o);
+    if(l,m,n,o>=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
