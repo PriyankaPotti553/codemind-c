ab609a5155fdec7ba0fbdd237b745ec82aca9b31
P.Priyanka <119730292+PriyankaPotti553@users.noreply.github.com>
2023-05-19 14:06:24 +0530

    #include<stdio.h> int main(){     int first = 0,second = 1,next = 0,n;     scanf("%d",&n);     while(n>next){         next=first+second;         first=second;         second=next;     }     if(n-first<second-n){         printf("%d",first);     }     else if(n-first==second-n){         printf("%d %d",first,second);     }     else{         printf("%d",second);     } }

--- nearest_fibonacci.c
+++ nearest_fibonacci.c
@@ -1,61 +1,19 @@
 #include<stdio.h>
-int fab(int n);
-int main()
-{
-    int n,d1,d2,ap,bp,i;
+int main(){
+    int first = 0,second = 1,next = 0,n;
     scanf("%d",&n);
-    fab(n);
-    for(i=n+1;;i++)
-    {
-        if(fab(i))
-        {
-            ap=i;
-            d1=i-n;
-            break;
-        }
+    while(n>next){
+        next=first+second;
+        first=second;
+        second=next;
     }
-    for(i=n-1;;i--)
-    {
-        if(fab(i))
-        {
-            bp=i;
-            d2=n-i;
-            break;
-        }
+    if(n-first<second-n){
+        printf("%d",first);
     }
-    if(d1<d2)
-    {
-        printf("%d",ap);
+    else if(n-first==second-n){
+        printf("%d %d",first,second);
     }
-    else if(d1>d2)
-    {
-        printf("%d",bp);
-    }
-    else
-    {
-        printf("%d %d",bp,ap);
+    else{
+        printf("%d",second);
     }
 }
-int fab(int n)
-{
-    int a=0,b=1,c,i,count=0;
-    for(i=1;i<=n;i++)
-    {
-        c=a+b;
-        a=b;
-        b=c;
-        if(c==n)
-        {
-            count=1;
-        }
-    }
-    if(count==1)
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-    
